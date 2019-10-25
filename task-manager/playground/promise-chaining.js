2eda7d0ef771b4f804bb5216f627d5cceeb213dd
Igor9n <id.igrec@gmail.com>
2019-10-25 13:08:28 +0300

    Finished course

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,12 +1,23 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-// 5da35162c3e71a216c234b70
+// User.findByIdAndUpdate('5c1a5a34d5a2ec046ca8f6bc', { age: 1 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
 
-User.findByIdAndUpdate('5da3516bc3e71a216c234b71', {age: 1}).then((user) => {
-    return User.countDocuments({age: 1})
-}).then((result) => {
-    console.log(result)
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('5c1a5a34d5a2ec046ca8f6bc', 2).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
